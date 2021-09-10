8169e90fc2fe3675673c55707da6f1b66d30cb3a
Jakub Lisiecki <jakub@lisiecki.xyz>
2021-09-10 13:46:11 +0200

    bootstrap added

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const path = require('path');
 module.exports = {
     entry: './src/js/script.js',
     output: {
-        publicPath: '/dist',
-        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
     },
 
     devServer: {
         static: {
-            directory: path.join(__dirname)
+            directory: path.join(__dirname, 'public')
         },
         compress: true,
         port: 8080,

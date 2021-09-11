62116b6287696dd1eb20034d0839b95b4136b405
JakubLisiecki <jakub@lisiecki.xyz>
2021-09-11 21:12:23 +0200

    compression + popper

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+const compression = require('compression');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(compression());
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
